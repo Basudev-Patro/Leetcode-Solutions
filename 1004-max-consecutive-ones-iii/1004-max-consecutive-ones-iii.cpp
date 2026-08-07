9c1f249508e3acac76330694d36171f8bae4434d
Basudev-Patro <basudevpatro406@gmail.com>
2026-08-07 23:33:01 +0530

    Time: 0 ms (100%), Space: 69.5 MB (65.8%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -3,7 +3,7 @@ public:
     int longestOnes(vector<int>& nums, int k) {
         int left = 0;
         int z_c = 0;
-        int max_c = 0;
+        int mx_cnt = 0;
 
         for(int i = 0; i < nums.size(); i++){
             if(nums[i] == 0){
@@ -12,11 +12,11 @@ public:
             while(z_c > k){
                 if(nums[left] == 0){
                     z_c--;
-                }
+                } 
                 left++;
             }
-            max_c = max(max_c,i - left + 1);
+            mx_cnt = max(mx_cnt, i - left + 1);
         }
-        return max_c;
+        return mx_cnt;
     }
 };
